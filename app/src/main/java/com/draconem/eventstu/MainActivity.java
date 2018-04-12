ac6d5e40e8f22e21108bc2fd2fc7802ffc7203cc
shervil <wintergpt45@gmail.com>
2018-04-12 23:55:08 +0530

    day2

--- app/src/main/java/com/draconem/eventstu/MainActivity.java
+++ app/src/main/java/com/draconem/eventstu/MainActivity.java
@@ -5,12 +5,18 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+
+public class MainActivity extends AppCompatActivity {
+    CardView bu1,bu2,bu3;
+    EditText t1,t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,41 @@ public class MainActivity extends AppCompatActivity
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        bu1 = (CardView)findViewById(R.id.but1);
+        bu2 = (CardView)findViewById(R.id.but2);
+        bu3 = (CardView)findViewById(R.id.but3);
+        t1 = (EditText) findViewById(R.id.name2);
+        t2 = (EditText) findViewById(R.id.pass);
+        bu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bu1.setVisibility(View.INVISIBLE);
+                bu1.setClickable(false);
+                bu2.setVisibility(View.INVISIBLE);
+                bu2.setClickable(false);
+                t1.setVisibility(View.VISIBLE);
+                t2.setVisibility(View.VISIBLE);
+                bu3.setVisibility(View.VISIBLE);
+                bu3.setClickable(true);
+                t1.setClickable(true);
+                t2.setClickable(true);
+            }
+        });
+        bu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bu1.setVisibility(View.INVISIBLE);
+                bu1.setClickable(false);
+                bu2.setVisibility(View.INVISIBLE);
+                bu2.setClickable(false);
+                t1.setVisibility(View.VISIBLE);
+                t2.setVisibility(View.VISIBLE);
+                bu3.setVisibility(View.VISIBLE);
+                bu3.setClickable(true);
+                t1.setClickable(true);
+                t2.setClickable(true);
+            }
+        });
     }
 
 }
